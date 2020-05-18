5f4e97f39451228660373d520b9ea4c3d8d199c8
LeeDongWook <lsy0566@naver.com>
2020-05-18 16:28:33 +0900

    0518

--- myapp-config-server/src/main/java/com/example/myappconfigserver/MyappConfigServerApplication.java
+++ myapp-config-server/src/main/java/com/example/myappconfigserver/MyappConfigServerApplication.java
@@ -1,15 +1,20 @@
 package com.example.myappconfigserver;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableConfigServer
 public class MyappConfigServerApplication {
 
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(MyappConfigServerApplication.class, args);
+
 	}
 
 }
